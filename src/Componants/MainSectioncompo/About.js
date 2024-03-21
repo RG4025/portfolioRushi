d5698ee27712bbe7f8332197908c3790aa80c068
Rushi <rgurav2311@gmail.com>
2024-03-21 17:01:30 +0530

    New Commit

--- src/Componants/MainSectioncompo/About.js
+++ src/Componants/MainSectioncompo/About.js
@@ -14,6 +14,161 @@ function About() {
               aliquam ipsum laudantium. Voluptates odio quisquam ab, deleniti
               eveniet perferendis, laudantium error inventore rerum molestias
               temporibus odit magnam officia!
+              lorem10
+              rupti dolores esse at! Veniam qui illum dolores rem! Alias,
+              provident excepturi! Atque consequatur non dignissimos ullam alias
+              accusantium? Exercitationem similique inventore laudantium iure
+              in, modi tempore, impedit alias ex nulla nihil quisquam ipsum eos
+              officitis aliquid consequuntur,
+              aliquam ipsum laudantium. Voluptates odio quisquam ab, deleniti
+              eveniet perferendis, laudantium error inventore rerum molestias
+              temporibus odit magnam officia!
+              lorem10
+              rupti dolores esse at! Veniam qui illum dolores rem! Alias,
+              provident excepturi! Atque consequatur non dignissimos ullam alias
+              accusantium? Exercitationem similique inventore laudantium iure
+              in, modi tempore, impedit alias ex nulla nihil quisquam ipsum eos
+              officitis aliquid consequuntur,
+              aliquam ipsum laudantium. Voluptates odio quisquam ab, deleniti
+              eveniet perferendis, laudantium error inventore rerum molestias
+              temporibus odit magnam officia!
+              lorem10
+              rupti dolores esse at! Veniam qui illum dolores rem! Alias,
+              provident excepturi! Atque consequatur non dignissimos ullam alias
+              accusantium? Exercitationem similique inventore laudantium iure
+              in, modi tempore, impedit alias ex nulla nihil quisquam ipsum eos
+              officitis aliquid consequuntur,
+              aliquam ipsum laudantium. Voluptates odio quisquam ab, deleniti
+              eveniet perferendis, laudantium error inventore rerum molestias
+              temporibus odit magnam officia!
+              lorem10
+              rupti dolores esse at! Veniam qui illum dolores rem! Alias,
+              provident excepturi! Atque consequatur non dignissimos ullam alias
+              accusantium? Exercitationem similique inventore laudantium iure
+              in, modi tempore, impedit alias ex nulla nihil quisquam ipsum eos
+              officitis aliquid consequuntur,
+              aliquam ipsum laudantium. Voluptates odio quisquam ab, deleniti
+              eveniet perferendis, laudantium error inventore rerum molestias
+              temporibus odit magnam officia!
+              lorem10
+              rupti dolores esse at! Veniam qui illum dolores rem! Alias,
+              provident excepturi! Atque consequatur non dignissimos ullam alias
+              accusantium? Exercitationem similique inventore laudantium iure
+              in, modi tempore, impedit alias ex nulla nihil quisquam ipsum eos
+              officitis aliquid consequuntur,
+              aliquam ipsum laudantium. Voluptates odio quisquam ab, deleniti
+              eveniet perferendis, laudantium error inventore rerum molestias
+              temporibus odit magnam officia!
+              lorem10
+              rupti dolores esse at! Veniam qui illum dolores rem! Alias,
+              provident excepturi! Atque consequatur non dignissimos ullam alias
+              accusantium? Exercitationem similique inventore laudantium iure
+              in, modi tempore, impedit alias ex nulla nihil quisquam ipsum eos
+              officitis aliquid consequuntur,
+              aliquam ipsum laudantium. Voluptates odio quisquam ab, deleniti
+              eveniet perferendis, laudantium error inventore rerum molestias
+              temporibus odit magnam officia!
+              lorem10
+              rupti dolores esse at! Veniam qui illum dolores rem! Alias,
+              provident excepturi! Atque consequatur non dignissimos ullam alias
+              accusantium? Exercitationem similique inventore laudantium iure
+              in, modi tempore, impedit alias ex nulla nihil quisquam ipsum eos
+              officitis aliquid consequuntur,
+              aliquam ipsum laudantium. Voluptates odio quisquam ab, deleniti
+              eveniet perferendis, laudantium error inventore rerum molestias
+              temporibus odit magnam officia!
+              lorem10
+              rupti dolores esse at! Veniam qui illum dolores rem! Alias,
+              provident excepturi! Atque consequatur non dignissimos ullam alias
+              accusantium? Exercitationem similique inventore laudantium iure
+              in, modi tempore, impedit alias ex nulla nihil quisquam ipsum eos
+              officitis aliquid consequuntur,
+              aliquam ipsum laudantium. Voluptates odio quisquam ab, deleniti
+              eveniet perferendis, laudantium error inventore rerum molestias
+              temporibus odit magnam officia!
+              lorem10
+              rupti dolores esse at! Veniam qui illum dolores rem! Alias,
+              provident excepturi! Atque consequatur non dignissimos ullam alias
+              accusantium? Exercitationem similique inventore laudantium iure
+              in, modi tempore, impedit alias ex nulla nihil quisquam ipsum eos
+              officitis aliquid consequuntur,
+              aliquam ipsum laudantium. Voluptates odio quisquam ab, deleniti
+              eveniet perferendis, laudantium error inventore rerum molestias
+              temporibus odit magnam officia!
+              lorem10
+              rupti dolores esse at! Veniam qui illum dolores rem! Alias,
+              provident excepturi! Atque consequatur non dignissimos ullam alias
+              accusantium? Exercitationem similique inventore laudantium iure
+              in, modi tempore, impedit alias ex nulla nihil quisquam ipsum eos
+              officitis aliquid consequuntur,
+              aliquam ipsum laudantium. Voluptates odio quisquam ab, deleniti
+              eveniet perferendis, laudantium error inventore rerum molestias
+              temporibus odit magnam officia!
+              lorem10
+              rupti dolores esse at! Veniam qui illum dolores rem! Alias,
+              provident excepturi! Atque consequatur non dignissimos ullam alias
+              accusantium? Exercitationem similique inventore laudantium iure
+              in, modi tempore, impedit alias ex nulla nihil quisquam ipsum eos
+              officitis aliquid consequuntur,
+              aliquam ipsum laudantium. Voluptates odio quisquam ab, deleniti
+              eveniet perferendis, laudantium error inventore rerum molestias
+              temporibus odit magnam officia!
+              lorem10
+              rupti dolores esse at! Veniam qui illum dolores rem! Alias,
+              provident excepturi! Atque consequatur non dignissimos ullam alias
+              accusantium? Exercitationem similique inventore laudantium iure
+              in, modi tempore, impedit alias ex nulla nihil quisquam ipsum eos
+              officitis aliquid consequuntur,
+              aliquam ipsum laudantium. Voluptates odio quisquam ab, deleniti
+              eveniet perferendis, laudantium error inventore rerum molestias
+              temporibus odit magnam officia!
+              lorem10
+              rupti dolores esse at! Veniam qui illum dolores rem! Alias,
+              provident excepturi! Atque consequatur non dignissimos ullam alias
+              accusantium? Exercitationem similique inventore laudantium iure
+              in, modi tempore, impedit alias ex nulla nihil quisquam ipsum eos
+              officitis aliquid consequuntur,
+              aliquam ipsum laudantium. Voluptates odio quisquam ab, deleniti
+              eveniet perferendis, laudantium error inventore rerum molestias
+              temporibus odit magnam officia!
+              lorem10
+              rupti dolores esse at! Veniam qui illum dolores rem! Alias,
+              provident excepturi! Atque consequatur non dignissimos ullam alias
+              accusantium? Exercitationem similique inventore laudantium iure
+              in, modi tempore, impedit alias ex nulla nihil quisquam ipsum eos
+              officitis aliquid consequuntur,
+              aliquam ipsum laudantium. Voluptates odio quisquam ab, deleniti
+              eveniet perferendis, laudantium error inventore rerum molestias
+              temporibus odit magnam officia!
+              lorem10
+              rupti dolores esse at! Veniam qui illum dolores rem! Alias,
+              provident excepturi! Atque consequatur non dignissimos ullam alias
+              accusantium? Exercitationem similique inventore laudantium iure
+              in, modi tempore, impedit alias ex nulla nihil quisquam ipsum eos
+              officitis aliquid consequuntur,
+              aliquam ipsum laudantium. Voluptates odio quisquam ab, deleniti
+              eveniet perferendis, laudantium error inventore rerum molestias
+              temporibus odit magnam officia!
+              lorem10
+              rupti dolores esse at! Veniam qui illum dolores rem! Alias,
+              provident excepturi! Atque consequatur non dignissimos ullam alias
+              accusantium? Exercitationem similique inventore laudantium iure
+              in, modi tempore, impedit alias ex nulla nihil quisquam ipsum eos
+              officitis aliquid consequuntur,
+              aliquam ipsum laudantium. Voluptates odio quisquam ab, deleniti
+              eveniet perferendis, laudantium error inventore rerum molestias
+              temporibus odit magnam officia!
+              lorem10
+              rupti dolores esse at! Veniam qui illum dolores rem! Alias,
+              provident excepturi! Atque consequatur non dignissimos ullam alias
+              accusantium? Exercitationem similique inventore laudantium iure
+              in, modi tempore, impedit alias ex nulla nihil quisquam ipsum eos
+              officitis aliquid consequuntur,
+              aliquam ipsum laudantium. Voluptates odio quisquam ab, deleniti
+              eveniet perferendis, laudantium error inventore rerum molestias
+              temporibus odit magnam officia!
+              lorem10
+              lorem
             </p>
     </>
   )
