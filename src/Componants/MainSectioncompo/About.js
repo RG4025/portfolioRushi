a8a8a698ede36b1bc8d348728c138d2f961bb37b
Rushi <rgurav2311@gmail.com>
2024-03-23 15:43:40 +0530

    New Commit

--- src/Componants/MainSectioncompo/About.js
+++ src/Componants/MainSectioncompo/About.js
@@ -1,6 +1,29 @@
 import React from "react";
-
+import * as icon from "react-bootstrap-icons";
 function About() {
+  const aboutMain = [
+    {
+      icons: <icon.CodeSquare />,
+      aboutTitle: "Frontend Dev",
+      aboutContent: "lorem ipsum doller sit ammet.",
+    },
+    {
+      icons: <icon.CodeSlash />,
+      aboutTitle: "Backend Dev",
+      aboutContent: "lorem ipsum doller sit ammet.",
+    },
+    {
+      icons: <icon.CodeSlash />,
+      aboutTitle: "API Integration",
+      aboutContent: "lorem ipsum doller sit ammet.",
+    },
+    {
+      icons: <icon.CodeSquare />,
+      aboutTitle: "Version Control",
+      aboutContent: "lorem ipsum doller sit ammet.",
+    },
+  ];
+
   return (
     <>
       <section>
@@ -10,11 +33,10 @@ function About() {
           </div>
           <div className="py-2">
             <p>
-              Hailing from Sangli, India, I'm a passionate Full Stack Developer
-              with a strong foundation in both frontend and backend
-              technologies. My focus is on crafting user-friendly interfaces
-              that are not only visually appealing but also intuitive and easy
-              to navigate.
+              I'm a passionate Full Stack Developer from Kolhapur with a strong
+              foundation in both frontend and backend technologies. My focus is
+              on crafting user-friendly interfaces that are not only visually
+              appealing but also intuitive and easy to navigate.
             </p>
 
             <p>
@@ -33,8 +55,26 @@ function About() {
             <h5 className="aboutTitle">I Work On</h5>
           </div>
 
-          <div>
-              
+          <div className="container">
+            <div className="row row-cols-1 row-cols-md-2">
+              {aboutMain.map((e) => {
+                return (
+                  <div className="col py-3 ">
+                    <div className="container">
+                      <div className="p-3 aboutChildTwoParent text-center d-flex justify-content-evenly  align-items-center ">
+                        <div className="aboutChildTwo">{e.icons}</div>
+                        <div>
+                          <strong className="fs-6 text-light">
+                            {e.aboutTitle}
+                          </strong>
+                          <p className="fs-6">{e.aboutContent}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </section>
